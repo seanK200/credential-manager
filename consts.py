e82a8d2e16addbed050680593bb6c6b5d3bcac52
Youngwoo Kim <yw.sean.kim@gmail.com>
2021-11-29 21:47:02 +0900

    refactor: clean up constants

--- consts.py
+++ consts.py
@@ -1,6 +1,6 @@
 import os
 
-# STRINGS
+# USER PROMPTS
 PROMPT_CMD=">>"
 PROMPT_MASTER_PW = "Enter master password: "
 PROMPT_CURRENT_MASTER_PW = "Enter old master password: "
@@ -8,19 +8,39 @@ PROMPT_NEW_MASTER_PW = "Enter new master password: "
 PROMPT_CONFIRM_PW = "Re-enter password: "
 PROMPT_USERNAME = "Enter username: "
 
+PROMPT_NEW_ENTRY_TITLE = "===== Adding new credential entry =====\n"
+PROPMT_NEW_ENTRY_NAME = "[1/3] Enter service/domain name: "
+PROPMT_NEW_ENTRY_ID = "[2/3] Enter your user ID/email (leave blank if there is none): "
+PROMPT_NEW_ENTRY_EMPTY_ID_CONFIRM = "You have not entered anything. Are you sure (y/n)? "
+PROMPT_NEW_ENTRY_PASSWORD_1 = "[3/3] Password\n"
+PROMPT_NEW_ENTRY_PASSWORD_2 = """<Options>
+1. Generate new secure password (recommended)
+2. Enter a pasword yourself\n
+Choose option (Enter 1 or 2): """
+PROMPT_NEW_ENTRY_PASSWORD_USER = "Enter a password for this service: "
+
+# RESPONSE MESSAGES
 WELCOME_USER = "Welcome, {}"
 
+# SUCCESS MESSAGES
 SUCCESS_NEW_MASTER_PW = "New master password successfully set."
 
+# ERROR MESSAGES
 ERROR_PW_CONFIRM = "Passwords do not match. Please try again."
-ERROR_PW_TOO_SHORT = "A password needs to be at least 6 characters long. Please try again."
+ERROR_PW_TOO_SHORT = "A password needs to be at least {} characters long. Please try again."
 ERROR_PW_UNSUPPORTED_CHARS = "Your password contains unsupported character(s). Please try again."
 ERROR_PW_EXCEED_MAX_ATTEMPTS = "You got the password wrong too many times. Exiting..."
 ERROR_MASTER_PW_WRONG = "Incorrect master password."
+
 ERROR_USERNAME_EMPTY = "A username cannot be empty."
 ERROR_USERNAME_TOO_SHORT = "A username must be at least 3 characters long."
 ERROR_USERNAME_UNSUPPORTED_CHARS = "Your username contains unsupported character(s). Please try again."
 
+ERROR_NEW_ENTRY_NAME_EMPTY = "You must enter a non-empty service/domain name."
+ERROR_NEW_ENTRY_NAME_TOO_LONG = "A service/domain name must be shorter than 512 characters. Please try again."
+ERROR_NEW_ENTRY_PW_INVALID_OPTIONS = "Enter either 1 or 2 to select an option. Please try again."
+
+# PATHS TO DIRECTORIES, FILES
 PATH_DATADIR = 'data'
 
 NAME_KEYFILE = 'master.key'
@@ -45,6 +65,11 @@ SCRYPT_MAX_MEM = 64 * 2 ** 20
 SCRYPT_DKLEN = 32
 
 MASTER_PW_MAX_ATTEMPTS = 5
+MASTER_PW_MIN_LEN = 6
+
+USERNAME_MIN_LEN = 3
+
+USER_PW_MIN_LEN = 6
 
 # COMMANDS (and their aliases)
 CMD_NEW = ['new', 'n']
@@ -54,3 +79,12 @@ CMD_DEL = ['delete', 'del', 'd', 'remove']
 CMD_LS = ['list', 'ls']
 CMD_LOCK = ['lock']
 CMD_EXIT = ['exit', 'quit']
+
+# RESPONSES (Answer to y/n prompts)
+RESPONSE_YES = ['yes', 'y']
+RESPONSE_NO = ['no', 'n']
+
+# MISC
+PRINTABLE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+    abcdefghijklmnopqrstuvwxyz0123456789\
+        !"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
