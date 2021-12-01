910b18ed3a11588a73482aa99bc2b93a970f3179
Youngwoo Kim <yw.sean.kim@gmail.com>
2021-12-02 01:20:26 +0900

    refactor: consolidate sql queries

---
+++ modules/sqlite_queries.py
@@ -0,0 +1,2 @@
+SQL_CREATE_TABLE = 'CREATE TABLE IF NOT EXISTS credentials(entry_id INTEGER PRIMARY KEY, \
+            name TEXT, user_id TEXT, user_pw BLOB, date_created TEXT, date_modified TEXT)'
